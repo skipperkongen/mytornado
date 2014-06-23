2a71dd8166bae3204cb1ce20071478e585f3f798
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2014-06-23 18:17:39 +0200

    fixed default app path bug

--- code/start_server.py
+++ code/start_server.py
@@ -5,7 +5,7 @@ from tornado.options import define, options, parse_command_line
 from mytornado import MyTornadoServer
 
 define("port", default=8080, help="run on the given port", type=int)
-define("app", default=os.path.join(os.path.dirname(__file__)), help="directory that contains the handlers/templates/static subdirectories", type=str)
+define("app", default=os.path.dirname(os.path.abspath(__file__)), help="directory that contains the handlers/templates/static subdirectories", type=str)
 
 if __name__ == '__main__':
 	parse_command_line()
