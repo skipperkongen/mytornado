4786e16b7cc747794d85cdd919714986b28510cd
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2014-06-23 21:38:03 +0200

    pushing stuff about

--- start_server.py
+++ start_server.py
@@ -7,7 +7,7 @@ from mytornado.server import Server
 define("port", default = 8080, help="run on the given port", type = int)
 define(
 	"app", 
-	default = os.path.join(os.path.dirname(os.path.abspath(__file__)), "default_app"), 
+	default = os.path.join(os.path.dirname(os.path.abspath(__file__)), "apps", "hello"), 
 	help = "directory that contains the handlers/templates/static subdirectories", 
 	type = str
 )
